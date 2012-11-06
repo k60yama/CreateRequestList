4ed9d7191c1bc1a9c748dde454ed5f3613364809
k60yama <k60yama@gmail.com>
2012-11-06 17:30:53 +0900

    Charctor Search

--- src/com/example/createrequestlist/MainMenu.java
+++ src/com/example/createrequestlist/MainMenu.java
@@ -2,6 +2,8 @@ package com.example.createrequestlist;
 
 
 
+import java.io.UnsupportedEncodingException;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -88,15 +90,60 @@ public class MainMenu extends Activity implements OnClickListener{
 		
 		Cursor cursor = db.query("product_info", COLUMNS, null, null, null, null, null);
     	
+		int count = 1;
 		while(cursor.moveToNext()){
         	
         	//Toast.makeText(this, cursor.getInt(0),Toast.LENGTH_SHORT).show();
-        	Toast.makeText(this, cursor.getString(1),Toast.LENGTH_SHORT).show();
-        	Toast.makeText(this, cursor.getString(2),Toast.LENGTH_SHORT).show();
+			
+			if(isWindows31j(cursor.getString(1))){
+				Toast.makeText(this, count + "文字コードはWindows31j",Toast.LENGTH_SHORT).show();
+			}else if(isSJIS(cursor.getString(1))){
+				Toast.makeText(this, count + "文字コードはSJIS",Toast.LENGTH_SHORT).show();
+			}else if(isEUC(cursor.getString(1))){
+				Toast.makeText(this, count + "文字コードはEUC",Toast.LENGTH_SHORT).show();
+			}else if(isUTF8(cursor.getString(1))){
+				Toast.makeText(this, count + "文字コードはUTF8",Toast.LENGTH_SHORT).show();
+			}else{
+				Toast.makeText(this, count + "該当する文字コードはありません",Toast.LENGTH_SHORT).show();
+			}
+			
+			count = count + 1;
+			
+        	//Toast.makeText(this, cursor.getString(2),Toast.LENGTH_SHORT).show();
         }
 		
 		//ここまでテスト
 		
     }
+    
+    
+    private static boolean checkCharacterCode(String str, String encoding) {
+		if (str == null) {
+			return true;
+		}
+
+		try {
+			byte[] bytes = str.getBytes(encoding);
+			return str.equals(new String(bytes, encoding));
+		} catch (UnsupportedEncodingException ex) {
+			throw new RuntimeException("エンコード名称が正しくありません。", ex);
+		}
+	}
+
+	public static boolean isWindows31j(String str) {
+		return checkCharacterCode(str, "Windows-31j");
+	}
+
+	public static boolean isSJIS(String str) {
+		return checkCharacterCode(str, "SJIS");
+	}
+
+	public static boolean isEUC(String str) {
+		return checkCharacterCode(str, "euc-jp");
+	}
+
+	public static boolean isUTF8(String str) {
+		return checkCharacterCode(str, "UTF-8");
+	}
     
 }
