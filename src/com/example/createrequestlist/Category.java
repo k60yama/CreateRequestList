8bad6bbb4da18494e411af3fd661eeaa73fc8770
k60yama <k60yama@gmail.com>
2012-11-16 23:50:30 +0900

    Visibility Control

--- src/com/example/createrequestlist/Category.java
+++ src/com/example/createrequestlist/Category.java
@@ -36,8 +36,14 @@ public class Category extends Activity{
 	//Buttonオブジェクトに設定するタグ用カウント
 	private Integer tagCount;
 	
-	//カテゴリ別にArrayListを作成
+	//カテゴリカウンター
+	private Integer categoryCount;
+	
+	//生成したEditTextをArrayListに格納
 	private ArrayList<EditText> edit = new ArrayList<EditText>();
+	
+	//生成したTableRowをArrayListに格納
+	private ArrayList<TableRow> tRow = new ArrayList<TableRow>();
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -53,15 +59,25 @@ public class Category extends Activity{
 
 	//カテゴリのボタン押下した場合
 	public void orderItemInfo(View view){
-		
 		//Button型にキャスト
-		//Button btn = (Button)view;
+		Button categoryBtn = (Button)view;
 		
 		//表示するカテゴリ名称取得
-		//String categoryView = btn.getText().toString();
+		String categoryName = categoryBtn.getText().toString();
 		
-		//表示・非表示の制御は後で考える
-		
+		//表示・非表示の制御(ArrayListに格納したTableRow分繰り返す)
+		String categoryTag;
+		for(int i = 0; i < tRow.size(); i++){
+			//TableRowに設定したタグを取得
+			categoryTag = (tRow.get(i)).getTag().toString();
+			
+			//タグと押下されたカテゴリの一致確認
+			if(categoryTag.equals(categoryName)){
+				tRow.get(i).setVisibility(View.VISIBLE); //表示
+			}else{
+				tRow.get(i).setVisibility(View.GONE);	//非表示
+			}
+		}
 	}
 	
 	//トップページへ押下した場合
@@ -88,11 +104,13 @@ public class Category extends Activity{
 		
 		//(EditText)タグ用カウント初期化
 		tagCount = 0;
+		categoryCount = 0;
 		
 		//カテゴリ数分繰り返す
 		for(String categoryName:CATEGORIES){
 			//TableLayout生成
 			this.createTable(categoryName);
+			categoryCount = categoryCount + 1;
 		}
 		
 		//データベース閉じる
@@ -111,11 +129,16 @@ public class Category extends Activity{
 		//0件の場合:TextView生成
 		//1件以上の場合:TableLayout生成
 		if(cursor.moveToFirst()){
-			
 			//レコード数分作成する
 			do{
 				//TableRow生成
 				TableRow row = new TableRow(this);
+				row.setTag(categoryName);
+				
+				//初期表示用
+				if(categoryCount != 0){
+					row.setVisibility(View.GONE);
+				}
 				
 				//CheckBox
 				CheckBox chk = new CheckBox(this);
@@ -144,6 +167,9 @@ public class Category extends Activity{
 				//プラスするButtonオブジェクトを追加
 				row.addView(this.createPlusButton());
 				
+				//TableRowをArrayListに追加
+				tRow.add(row);
+				
 				//生成したTableRowをTableLayoutに追加
 				tLayout.addView(row);
 				
@@ -154,14 +180,24 @@ public class Category extends Activity{
 		}else{
 			//TableRow生成
 			TableRow row = new TableRow(this);
+			row.setTag(categoryName);
+			
+			//初期表示用
+			if(categoryCount != 0){
+				row.setVisibility(View.GONE);
+			}
 			
 			//TextView生成
 			TextView nothing = new TextView(this);
 			nothing.setText("選択されたカテゴリに品物は登録されていません。");
 			row.addView(nothing);
+			
+			//TableRowをArrayListに格納
+			tRow.add(row);
+			
+			//生成したTableRowをTableLayoutに追加
 			tLayout.addView(row);
 		}
-		
 		//検索結果クリア
 		cursor.close();
 	}
@@ -175,7 +211,7 @@ public class Category extends Activity{
 	private Button createMinusButton(){
 		//Button生成
 		Button mBtn = new Button(this);
-		mBtn.setWidth(100);
+		mBtn.setWidth(120);
 		mBtn.setText("マイナス");
 		mBtn.setTag(tagCount);
 		mBtn.setOnClickListener(new OnClickListener(){
@@ -198,12 +234,10 @@ public class Category extends Activity{
 				if(num > 0){
 					num = num - 1;
 				}
-				
 				//プラスした値をEditTextに再設定する
 				editNum.setText(String.valueOf(num));
 			}
 		});
-				
 		return mBtn;
 	}
 	
@@ -211,7 +245,7 @@ public class Category extends Activity{
 	private Button createPlusButton(){
 		//Button生成
 		Button pBtn = new Button(this);
-		pBtn.setWidth(100);
+		pBtn.setWidth(120);
 		pBtn.setText("プラス");
 		pBtn.setTag(tagCount);
 		pBtn.setOnClickListener(new OnClickListener(){
@@ -237,7 +271,6 @@ public class Category extends Activity{
 				editNum.setText(String.valueOf(num));
 			}
 		});
-		
 		return pBtn;
 	}
 	
